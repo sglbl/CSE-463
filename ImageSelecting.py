7c9e718171f603e447f29e602140fd55fda25e06
sglbl <silo1881@gmail.com>
2022-04-24 23:43:32 +0300

    Last

--- ImageSelecting.py
+++ ImageSelecting.py
@@ -22,18 +22,6 @@ class ImageSelector:
                 print("Both images are selected")
                 ObjectDetecting.ObjectDetector(self.images[0], self.images[1]) # Calling object detector
 
-            # cv.namedWindow('image')  # Putting name to window
-
-            # Showing the image
-            # while(1):
-            #     cv.imshow('image', self.images[imageNumber-1])
-            #     k = cv.waitKey(20) & 0xFF                   # Masking the value with 11111111 to get only 8 bits
-            #     if k == 27 or k == ord('q'):                # If the value is 27 (means ESC key is pressed) or q is pressed, then exit.
-            #         break
-            #     if( cv.getWindowProperty('image', cv.WND_PROP_VISIBLE) < 1 ): # If X button is pressed, then exit
-            #         break
-            # cv.destroyAllWindows()
-
     def filePathFinder(self):
         currdir = os.getcwd()
         tempdir = tkinter.filedialog.askopenfilename(initialdir=currdir, title='Please select a file')
--- ObjectDetecting.py
+++ ObjectDetecting.py
@@ -1,7 +1,4 @@
 import cv2 as cv
-import numpy as np       # Mathematical array operations
-import scipy.interpolate # to interpolate a matrix
-import tkinter as tk
 
 class ObjectDetector:
     def __init__(self, image1, image2) -> None:
@@ -12,22 +9,12 @@ class ObjectDetector:
         keyPoint1, descriptor1 = sift.detectAndCompute(grayscaleImage1, None)
         keyPoint2, descriptor2 = sift.detectAndCompute(grayscaleImage2, None)
 
-        # bestMatches = self.goodMatchFinder()
-        # finalImage = cv.drawMatches(grayscaleImage1, keyPoint1, grayscaleImage2, keyPoint2, bestMatches, None ,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-
         matches = self.matcherAndSorter(descriptor1, descriptor2)
-        finalImage= cv.drawMatches(grayscaleImage1, keyPoint1, grayscaleImage2, keyPoint2, matches[:100], None ,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        finalImage= cv.drawMatches(image1, keyPoint1, image2, keyPoint2, matches[:100], None ,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
         cv.imshow("Matches", finalImage)
 
         cv.waitKey(0)
-
-
-    def matcherAndSorter(self, descriptor1, descriptor2):
-        matches = cv.BFMatcher().match(descriptor1, descriptor2)
-        # Sort them in the order of their distance.
-        matches = sorted(matches, key = lambda x:x.distance)
-        return matches
 
     def imagesToGrayscale(self, image1, image2):
         # Converting images to grayscale for detection
@@ -35,12 +22,8 @@ class ObjectDetector:
         image2 = cv.cvtColor(image2, cv.COLOR_BGR2GRAY)
         return image1, image2
 
-    def goodMatchFinder(self, descriptor1, descriptor2):
-        # Using brute for match
-        matches = cv.BFMatcher().knnMatch(descriptor1, descriptor2, k=2)
-        goodMatches = []
-        # Find good matches by using distances
-        for m,n in matches:
-            if 0.70*n.distance > m.distance:
-                goodMatches.append(m)
-        return goodMatches
+    def matcherAndSorter(self, descriptor1, descriptor2):
+        matches = cv.BFMatcher().match(descriptor1, descriptor2)
+        # Sort them in the order of their distance.
+        matches = sorted(matches, key = lambda x:x.distance)
+        return matches
